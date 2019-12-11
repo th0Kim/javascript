b228c9b72bc6cc1e4603c50ed29136250ed1b6c6
soyoungKim <sy.kim58@hanmail.net>
2019-12-11 16:39:31 +0900

    styled component - 노마드코더

---
+++ styledComponents/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        retrun <button className="button">Hello</button>;
+    }
+}
+
+export default App;
